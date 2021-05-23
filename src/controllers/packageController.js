094f450247372965f9ac2f385a655192d6f511ba
Pedro Dominici <pedro.dominici@acct.global>
2021-05-23 15:46:08 -0300

    add frete price

--- src/controllers/packageController.js
+++ src/controllers/packageController.js
@@ -1,26 +1,82 @@
 //  BODY REQUESITION
 //  DIMENSOES EM CM
-//  PESO EM KG
-// http://localhost:3333/package?height=50&width=50&weight=2
+//  PESO EM GRAMAS
+// http://localhost:3333/package?height=50&width=50&weight=2&length=20
 
 const Utils = require("../utils/index");
 
 exports.get = (req, res, next) => {
-  // console.log(req.query);
+  const stateList = [
+    "SP",
+    "PR",
+    "RJ",
+    "SC",
+    "RS",
+    "MG",
+    "MS",
+    "GO",
+    "MT",
+    "TO",
+    "BH",
+    "ES",
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "CE",
+    "MA",
+    "PA",
+    "PB",
+    "PE",
+    "PI",
+    "RN",
+    "RO",
+    "RR",
+    "SE",
+    "DF",
+  ];
   const finalHeight = Utils.onlyNumbers(req.query.height);
   const finalWidth = Utils.onlyNumbers(req.query.width);
   const finalWeight = Utils.onlyNumbers(req.query.weight);
+  const finalLength = Utils.onlyNumbers(req.query.length);
+  const finalState = req.query.state.toUpperCase();
+  let stateChecked = false;
+
+  if (stateList.includes(finalState)) {
+    stateChecked = true;
+  }
+
   console.log(
     "Height: ",
     finalHeight,
     "Width: ",
     finalWidth,
     "Weight: ",
-    finalWeight
+    finalWeight,
+    "Length: ",
+    finalLength,
+    "state: ",
+    finalState
   );
-  if (finalHeight > 0 && finalWidth > 0 && finalWeight > 0) {
-    res.status(200).send("Sucesso!");
+
+  if (
+    finalHeight > 0 &&
+    finalWidth > 0 &&
+    finalWeight > 0 &&
+    finalLength > 0 &&
+    stateChecked
+  ) {
+    const frete = Utils.freteValue(
+      finalHeight,
+      finalWidth,
+      finalLength,
+      finalWeight,
+      finalState
+    );
+
+    res.status(200).send(`Sucesso! - Preço: ${frete}`);
     console.log("✅ PACKAGE GET ROUTE ACCESS");
+    console.log(frete);
   } else {
     res.status(400).send("Erro nos paramentros!");
     console.log("❌ PACKAGE GET ROUTE ACCESS");
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,47 @@ exports.onlyNumbers = (string) => {
   const numbers = string.replace(/([^\d])+/gim, "");
   return parseFloat(numbers);
 };
+
+exports.freteValue = (height, width, length, weight, state) => {
+  const low = ["SP", "PR", "RJ", "SC", "RS", "MG", "MS"];
+  const average = ["GO", "MT", "TO", "BH", "ES"];
+  const high = [
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "CE",
+    "MA",
+    "PA",
+    "PB",
+    "PE",
+    "PI",
+    "RN",
+    "RO",
+    "RR",
+    "SE",
+    "DF",
+  ];
+
+  let priceState = 0;
+
+  if (low.includes(state)) {
+    priceState = 15;
+  }
+  if (average.includes(state)) {
+    priceState = 50;
+  }
+  if (high.includes(state)) {
+    priceState = 80;
+  }
+
+  const priceByHeight = 1.66; // R$1,66 por cm
+  const priceByWidth = 1; // R$1 por cm
+  const priceByLength = 1; // R$1 por cm
+  const priceByWeight = 0.005; // R$ 0,005 por grama
+
+  const finalFrete =
+    height * priceByHeight * (width * priceByWidth) * (length * priceByLength) +
+    weight * priceByWeight;
+  return finalFrete / 1000 + priceState;
+};
