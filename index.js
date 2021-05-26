15b8c4447c6b6c56df0009da820ae1b8d91270e5
Pedro Dominici <pedro.dominici@acct.global>
2021-05-25 22:31:54 -0300

    add port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+var port = process.env.PORT || 3333;
 
 require("./src/routes/index")(app);
 
 app.use(cors());
 app.use(express.json());
-app.listen(3333, () => {
+app.listen(port, () => {
   console.log("🚀 API ON");
 });
