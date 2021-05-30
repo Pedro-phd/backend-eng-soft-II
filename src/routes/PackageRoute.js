ea57b8d8656bd10d4e51f4e7c5043c9f0b2ee155
Pedro Dominici <pedro.dominici@acct.global>
2021-05-30 12:23:40 -0300

    cors config

--- src/routes/PackageRoute.js
+++ src/routes/PackageRoute.js
@@ -1,5 +1,10 @@
 const PackageController = require("../controllers/packageController");
+var cors = require("cors");
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 module.exports = (app) => {
-  app.get("/package", PackageController.get);
+  app.get("/package", cors(corsOptions), PackageController.get);
 };
